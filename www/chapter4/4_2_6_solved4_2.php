93afd1c5ef9792b449be77e7d2233cfdee0fab6e
sjdonahu <snarlydogs@gmail.com>
2014-03-13 13:00:58 -0400

    new problems

--- www/chapter4/4_2_6_solved4_2.php
+++ www/chapter4/4_2_6_solved4_2.php
@@ -401,6 +401,8 @@
 							$$
 							\begin{align*}
 							EX 	&#38;= \int_0^\infty x f_X(x) dx \\
+							\end{align*}
+							$$
 								 	&#38;= \int_0^\infty x \cdot \frac{\lambda^{\alpha}}{\Gamma{\alpha}} x^{\alpha - 1} e^{-\lambda x} {\rm d}x 		\\
 								 	&#38;= \frac{\lambda^{\alpha}}{\Gamma{\alpha}} \int_0^{\infty} x \cdot x^{\alpha - 1} e^{-\lambda x} {\rm d}x 	\\
 								 	&#38;= \frac{\lambda^{\alpha}{\Gamma{\alpha}} \int_0^{\infty} x^{\alpha} e^{-\lambda x} {\rm d}x								\\
