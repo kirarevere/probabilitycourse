059553381f0c37d2d2e0ca73c125ab558e25fa10
sjdonahu <snarlydogs@gmail.com>
2014-03-22 13:35:16 -0400

    section header

--- www/chapter5/5_1_0_joint_distributions.php
+++ www/chapter5/5_1_0_joint_distributions.php
@@ -1,1 +1,7 @@
-<?php $section = "5.1.0"; include 'section_header.php' ?>
+<?php
+	$section_no = "5.1.0";
+	$section_title = "Joint Distributions: Two Random Variables";
+	$section_prev = "chapter4/4_4_0_chapter4_problems.php";
+	$section_next = "chapter5/5_1_1_joint_pmf.php";
+	include 'section_header.php' 
+?>
