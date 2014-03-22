e9a974dec8963a8884cf82b3e65e57f14cd3b517
sjdonahu <snarlydogs@gmail.com>
2014-03-22 17:18:48 -0400

    init 5.1.2

---
+++ www/chapter5/5_1_2_joint_cdf.php
@@ -0,0 +1,17 @@
+<?php
+	$section_no = "5.1.2";
+	$section_title = "Joint Cumulative Distributive Function (CDF)";
+	$section_prev = "chapter4/5_1_1_joint_pmf";
+	$section_next = " ";
+	include "../Control/autonumber.php";
+	loadHeaders(58);	
+	include "sectionHeader.php";
+?>
+
+<!-- Section Content -->
+
+<!-- End Section Content -->
+
+<?php
+	include "sectionFooter.php";	
+?>
