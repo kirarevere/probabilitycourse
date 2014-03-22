60a5627fc12e116dea282aa4defbbe4bb36cdebf
sjdonahu <snarlydogs@gmail.com>
2014-03-22 14:09:59 -0400

    pmf definition div

--- www/chapter5/5_1_1_joint_pmf.php
+++ www/chapter5/5_1_1_joint_pmf.php
@@ -9,11 +9,12 @@
 <!-- Section content -->
 Remember that for a discrete random variable $X$, we define the PMF as $P_X(x)=P(X=x)$. Now, if we have two random variables $X$ and $Y$, and we would like to study them jointly, we define the <b>joint probability mass function</b> as follows:
 
-<div style="border: 2px solid black;margin: 30px 40px;">
-<b>joint probability mass function</b> of two discrete random variables $X$ and $Y$ is defined as
+<div style="padding: 20px; border: 2px solid black;margin: 30px 40px;">
+The <b>joint probability mass function</b> of two discrete random variables $X$ and $Y$ is defined as
 \begin{align}%\label{}
 \nonumber P_{XY}(x,y)=P(X=x, Y=y).
 \end{align}
+</div>
 Note that as usual, the comma means ``and'', so we can write
 \begin{align}%\label{}
 \nonumber P_{XY}(x,y)&=P(X=x, Y=y) \\
@@ -23,7 +24,6 @@ We can define the joint range for $X$ and $Y$ as
 \begin{align}%\label{}
 \nonumber R_{XY}=\{(x,y) | P_{XY}(x,y)>0\}.
 \end{align}
-</div>
 
 <!-- end section content -->
 
