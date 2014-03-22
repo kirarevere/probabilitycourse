2ba550591f9d29ba0dbdc3203852aa1c95205081
sjdonahu <snarlydogs@gmail.com>
2014-03-22 18:06:30 -0400

    init 5.1.3

---
+++ www/chapter5/5_1_3_conditioning_independence.php
@@ -0,0 +1,17 @@
+<?php
+	$section_no = "5.1.3";
+	$section_title = "Conditioning and Independence";
+	$section_prev = " ";
+	$section_next = " ";
+	include "../Control/autonumber.php";
+	loadHeaders(59);
+	include "section_header.php";	
+?>
+
+<!-- Section Content -->
+
+<!-- End Section Content -->
+
+<?php
+	include "section_footer.php";
+?>
