d2f68946b0ac8459ef38f48b0e6009ee1d8b0807
sjdonahu <snarlydogs@gmail.com>
2014-03-22 13:43:53 -0400

    section footer

--- www/chapter5/5_1_0_joint_distributions.php
+++ www/chapter5/5_1_0_joint_distributions.php
@@ -3,5 +3,12 @@
 	$section_title = "Joint Distributions: Two Random Variables";
 	$section_prev = "chapter4/4_4_0_chapter4_problems.php";
 	$section_next = "chapter5/5_1_1_joint_pmf.php";
-	include 'section_header.php' 
+	include 'section_header.php'; 
+?>
+
+<!-- Section content -->
+
+<!-- End Section content -->
+<?php
+	include 'section_footer.php';
 ?>
---
+++ www/chapter5/section_footer.php
@@ -0,0 +1,16 @@
+			<div class="thinblock hide_print">
+				<div class="left"><a>&larr; </a><a href="<?php echo $section_prev ?>">previous</a></div>
+    			<div class="right"><a href="<?php echo $section_next ?">next</a><a> &rarr;</a></div>
+    			<div class="clear"></div>
+    		</div>		
+		</div>
+		<div id="menu" class="hide_print">
+			 <script type="text/javascript" src="javascript/menu_html_code.js"></script>
+		</div>
+
+		<div id="footer" class="hide_print">
+			<script type="text/javascript" src="http://probabilitycourse.com/javascript/footer_html_code.js"></script>
+		</div>
+	</div>
+</body>
+</html>
