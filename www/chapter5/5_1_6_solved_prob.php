792e84d459baf0f396ed7b1b907a6e3ff593de2d
sjdonahu <snarlydogs@gmail.com>
2014-03-24 18:34:49 -0400

    equation

--- www/chapter5/5_1_6_solved_prob.php
+++ www/chapter5/5_1_6_solved_prob.php
@@ -376,7 +376,7 @@ We solve this problem using a similar approach as in Example 5.6. Let $\mu=EX$. 
      \nonumber  &=2p+(2+\mu)(1-p)\\
      \nonumber  &=2+(1-p)\mu.
   \end{align}
-  Here, $E[X|HT]=2+EX$ because, if the first two tosses are $HT$, we have wasted two coin tosses and we start over at the third toss. By letting $E[X|H]=2+(1-p)\mu$ in Equation \ref{alhh}, we obtain
+  Here, $E[X|HT]=2+EX$ because, if the first two tosses are $HT$, we have wasted two coin tosses and we start over at the third toss. By letting $E[X|H]=2+(1-p)\mu$ in 5.14, we obtain
   \begin{align}
      \nonumber \mu=EX=\frac{1+p}{p^2}.
   \end{align}
