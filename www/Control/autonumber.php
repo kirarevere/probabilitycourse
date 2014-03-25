2fd5c1f8368193bbd004ea9501918bc2d84b1ad5
sjdonahu <snarlydogs@gmail.com>
2014-03-24 22:54:52 -0400

    error reporting

--- www/Control/autonumber.php
+++ www/Control/autonumber.php
@@ -124,6 +124,7 @@
 			$filename = substr($file,strlen($phpDir));
 			$fileInfo = sscanf($filename,"%d_%d_%d_%s");
 			list($ch,$sec,$subsec,$name) = $fileInfo;
+			echo "$ch.$sec.$subsec $name\n";
 			$dom->loadHTMLFile($file);
 			foreach($dom->getElementsByTagName('span') as $tag)	{
 				if(stripos($tag->getAttribute('class'),"example") !== false) {
