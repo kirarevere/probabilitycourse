bac74cad71294189f741da5e634cbe69cd4b2857
sjdonahu <snarlydogs@gmail.com>
2014-03-25 15:37:08 -0400

    5.2.2

--- www/chapter5/5_2_2_joint_cdf.php
+++ www/chapter5/5_2_2_joint_cdf.php
@@ -3,7 +3,7 @@
 	$section_title	= "Joint Cumulative Distribution Function (CDF)";
 	$section_prev		= "chapter5/5_2_1_joint_pdf.php";
 	$section_next		= "chapter5/5_2_3_";
-	include						"Control/autonumber.php";
+	include						"../Control/autonumber.php";
 	loadHeader(67);
 	include						"section_header.php";
 ?>
