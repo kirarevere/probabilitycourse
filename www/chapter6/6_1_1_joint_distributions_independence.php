cd45c5747490b2df64d3428e549a7847b50a6e59
sjdonahu <snarlydogs@gmail.com>
2014-04-10 01:05:58 -0400

    init chapter6

---
+++ www/chapter6/6_1_1_joint_distributions_independence.php
@@ -0,0 +1,33 @@
+<?php
+	$section_no			= "6.1.1";
+	$section_title	= "Joint Distributions and Independence";
+	$section_prev		= "chapter6/6_1_0_multiple_random_variables.php";
+	$section_next		= "chapter6/#";
+	include						"../Control/autonumber.php";
+	loadHeaders(74);
+	include						"section_header.php";
+?>
+
+<!-- Section Content -->
+<p>
+For three or more random variables, the joint PDF, joint PMF, and joint CDF are defined in a similar way to what we have already seen for the case of two random variables. Let $X_1$, $X_2$, $\cdots$, $X_n$ be $n$ discrete random variables. The joint PMF of  $X_1$, $X_2$, $\cdots$, $X_n$ is defined as
+\begin{align}%\label{}
+\nonumber  P_{X_1,X_2,...,X_n}(x_1,x_2,...,x_n)=P(X_1=x_1,X_2=x_2,...,X_n=x_n).
+\end{align}
+For $n$ jointly continuous random variables $X_1$, $X_2$, $\cdots$, $X_n$, the joint PDF is defined to be the function $f_{X_1X_2...X_n}(x_1,x_2,...,x_n)$ such that the probability of any set $A \subset \mathbb{R}^n$ is given by the integral of the PDF over the set $A$. In particular, for a set $A \in \mathbb{R}^n$, we can write
+\begin{align}%\label{}
+\nonumber  P\bigg((X_1,X_2,\cdots,X_n) \in A \bigg)=\int \cdots\int \limits_A\cdots  \int  f_{X_1X_2 \cdots X_n}(x_1,x_2, \cdots ,x_n)\hs dx_1dx_2 \cdots dx_n.
+\end{align}
+The marginal PDF of $X_i$ can be obtained by integrating all other $X_j$'s. For example,
+\begin{align}%\label{}
+\nonumber  f_{X_1}(x_1)=\int_{-\infty}^{\infty} \cdots \int_{-\infty}^{\infty}   f_{X_1X_2...X_n}(x_1,x_2,...,x_n)\hs dx_2 \cdots dx_n.
+\end{align}
+The joint CDF of $n$ random variables $X_1$, $X_2$,...,$X_n$ is defined as
+\begin{align}%\label{}
+\nonumber  F_{X_1,X_2,...,X_n}(x_1,x_2,...,x_n)=P(X_1 \leq x_1,X_2 \leq x_2,...,X_n \leq x_n).
+\end{align}
+
+</p>
+<!-- /Section Content -->
+
+<?php include 'section_footer.php'; ?>
---
+++ www/chapter6/section_header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<base href="/" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<!-- Lets autonumber track countable elements for this section -->
+	<meta name="section" content="<?php echo $section_no ?>" />
+
+	<!-- Configuration for LaTeX rendering -->
+	<script type="text/x-mathjax-config">
+  			MathJax.Hub.Config({
+				"HTML-CSS": {
+				scale: "125"
+				},
+    		tex2jax: { inlineMath: [['$','$'],['\\(','\\)']] }
+  			});
+	</script>	
+
+	<!-- MathJax runtime script -->
+	<script type="text/javascript"
+  			src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+	</script>
+	
+	<!-- Javascript imports -->
+	<script type='text/javascript' src='http://code.jquery.com/jquery-1.6.3.js'></script>
+	<script type="text/javascript" src="http://probabilitycourse.com/javascript/menu_js_code.js"></script>
+	<script type="text/javascript" src="http://probabilitycourse.com/javascript/solved_prob.js"></script>
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="http://probabilitycourse.com/style_sheet.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="print.css" media="print" />
+
+	<!-- Page Title -->
+	<title><?php echo $section_title ?></title>
+
+</head>
+<body>
+<div id="container">
+		<span class="hide_print">
+			<script type="text/javascript" src="http://probabilitycourse.com/javascript/top_page_html_code.js"></script>
+		</span>
+		
+		<div id="wrapper">
+			<div id="content">
+			<div class="thinblock hide_print">
+				<div class="left"><a>&larr; </a><a href="<?php echo $section_prev ?>">previous</a></div>
+    			<div class="right"><a href="<?php echo $section_next ?>">next</a><a> &rarr;</a></div>
+    			<div class="clear"></div>
+    		</div>
+			<hr />
+						
+			<div class="left"><h2><?php echo "$section_no $section_title" ?></h2></div>
+    		<div class="right hide_print">
+<!--
+			<a href="http://probabilitycourse.com/videos/chapter4/video4_2.html"><img src="http://probabilitycourse.com/images/video_icon_lg.png" alt="Video Available" title="Video Available" border="0" style="vertical-align: middle;"/></a>
+-->
+				</div><!-- end right -->
+    		<div class="clear"></div>
+
+
