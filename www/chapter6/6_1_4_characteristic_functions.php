21d9951eaa95bdfab25d193a0f685bce2d4aaa86
sjdonahu <snarlydogs@gmail.com>
2014-04-10 02:47:03 -0400

    6.1.4 typos

--- www/chapter6/6_1_4_characteristic_functions.php
+++ www/chapter6/6_1_4_characteristic_functions.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- section content -->
-There are random variables for which the moment generating function does not exist on any real interval with positive length. For example, consider the random variable $X$ that has a \textit{Cauchy} distribution
+There are random variables for which the moment generating function does not exist on any real interval with positive length. For example, consider the random variable $X$ that has a <i>Cauchy</i> distribution
 \begin{align}%\label{}
    f_X(x)=\frac{\frac{1}{\pi}}{1+x^2}, \textrm{for all }x \in \mathbb{R}.
 \end{align}
@@ -21,11 +21,11 @@ Therefore, the moment generating function does not exist for this random variabl
 
 If a random variable does not have a well-defined MGF, we can use the characteristic function defined as
 \begin{align}%\label{}
- \no  \phi_{X}(\omega)&=E[e^{j \omega X}],
+  \phi_{X}(\omega)&=E[e^{j \omega X}],
 \end{align}
 where $j=\sqrt{-1}$ and $\omega$ is a real number. It is worth noting that $e^{j \omega X}$ is a complex-valued random variable. We have not discussed complex-valued random variables. Nevertheless, you can imagine that a complex random variable can be written as $X=Y+jZ$, where $Y$ and $Z$ are ordinary real-valued random variables. Thus, working with a complex random variable is like working with two real-valued random variables.  The advantage of the characteristic function is that it is defined for all real-valued random variables. Specifically, if $X$ is a real-valued random variable, we can write
 \begin{align}%\label{}
-   \no  |e^{j \omega X}|=1.
+    |e^{j \omega X}|=1.
 \end{align}
 Therefore, we conclude
 \begin{align}%\label{}
